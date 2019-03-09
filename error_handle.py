5390d4251ae991c818d0ca06231d469f10f199e6
Peter N Jenkin <peternjenkin@gmail.com>
2019-03-09 11:40:49 +0000

    specific divide by zero error handling

--- error_handle.py
+++ error_handle.py
@@ -2,7 +2,7 @@ def divide(a,b):
     """ this method is meant to be called making a deliberate error, caught in a try block """
     try:
         return a / b
-    except:
+    except ZeroDivisionError:
         print('Divide by zero error caught')
 
 
